604b3ecea48dc935ff90c7ba5bd81f18333b4381
Yash Jangra <urmilajangra303@gmail.com>
2026-09-11 18:05:56 +0530

    feat:add gateway routes for shipment and notification services

---
+++ src/test/java/com/shipment/smartapigateway/SmartApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shipment.smartapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartApiGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
